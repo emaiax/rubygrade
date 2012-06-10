996938a1dfbc7a21897105f8c9b24d83143dee00
Eduardo Maia <eduvimaia@gmail.com>
2012-06-10 05:38:02 -0300

    upgrade para Rails 3.x

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubygrade::Application.config.secret_token = '8b6921a93c7551de01293670548fdb3880ec03bff0cd79a8df7d1a73be5a79a3405529f93ba66f0069c1f1ad9cdb1f09f55dacbdbceba7a610b5f848d1368426'
